aeff2e175d91970226186303a2e1a9a9bb259c95
nagelk <nagelk@K141-4.dep-info.iutmontp.univ-montp2.fr>
2023-11-06 09:40:22 +0100

    feat(Document.java): fonctionnalite majuscule
    feat: commande majuscule
    
    feat: prise en compte de la commande majuscule

---
+++ src/main/java/fr/iut/editeur/commande/CommandeMajusucule.java
@@ -0,0 +1,23 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeMajusucule extends CommandeDocument {
+
+    public CommandeMajusucule(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer(){
+        if(parameters.length < 2) {
+            System.err.println("Format attendu : majuscules;indexDebut;indexFin");
+            return;
+        }
+        int indexStart = Integer.parseInt(parameters[1]);
+        int indexEnd = Integer.parseInt(parameters[2]);
+
+        this.document.majuscules(indexStart,indexEnd);
+        super.executer();
+    }
+}
