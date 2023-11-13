26f3215f41d63f57f32b5b56361bfceb3ee301f3
kilian-nagel <nagelkilian05@gmail.com>
2023-11-13 12:38:41 +0100

    test: test méthode ajouter

---
+++ src/test/java/fr/iut/editeur/DocumentTest.java
@@ -0,0 +1,14 @@
+package fr.iut.editeur;
+
+import fr.iut.editeur.document.Document;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class DocumentTest extends TestCase {
+    @Test
+    public void testAjouter(){
+        Document d = new Document();
+        d.ajouter("hi");
+        assertEquals("hi",d.toString());
+    }
+}
